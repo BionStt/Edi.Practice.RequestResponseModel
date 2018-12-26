7e63e03aab95666464dafd8bceed4a728a6855bf
Edi Wang <Edi.Wang@outlook.com>
2018-12-26 13:44:40 +0800

    init commit

---
+++ src/Edi.Practice.RequestResponseModel/Request.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Edi.Practice.RequestResponseModel
+{
+    public class Request
+    {
+        public string RequestId { get; set; }
+
+        public DateTimeOffset RequestTime { get; set; }
+    }
+
+    public class Request<T> : Request
+    {
+        public T Item { get; set; }
+
+        public Request()
+        {
+
+        }
+
+        public Request(T item)
+        {
+            Item = item;
+        }
+    }
+}
